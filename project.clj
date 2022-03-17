8daabf2ea4535f2802802c30d03685ac8fe63c2f
Sergey <sergey@minorodata.com>
2022-03-17 11:45:51 +0300

    prepare to deployment

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject deburger "0.1.0"
+(defproject org.clojars.tka4enko/deburger "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -12,4 +12,14 @@
   [["clojars" {:sign-releases false
                :url           "https://clojars.org/repo"
                :username      :env/CLOJARS_USERNAME
-               :password      :env/CLOJARS_PASSWORD}]])
+               :password      :env/CLOJARS_PASSWORD}]]
+
+  :release-tasks
+  [["vcs" "assert-committed"]
+   ["change" "version" "leiningen.release/bump-version" "release"]
+   ["vcs" "commit"]
+   ["vcs" "tag" "--no-sign"]
+   ["deploy" "clojars"]
+   ["change" "version" "leiningen.release/bump-version"]
+   ["vcs" "commit"]
+   ["vcs" "push"]])
